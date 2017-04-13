f6a234bd062debdb3b6c37b7e64197f87633ea02
soyunlee882 <soyunlee882@gmail.com>
2017-04-13 20:18:28 +0900

    #3 git commit -m #3

--- app/src/main/java/syl/kr/hs/emirim/activitytest/MainActivity.java
+++ app/src/main/java/syl/kr/hs/emirim/activitytest/MainActivity.java
@@ -1,13 +1,29 @@
 package syl.kr.hs.emirim.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button butMain=(Button)findViewById(R.id.but_main);
+        butMain.setOnClickListener(butMainHandler);
     }
+    View.OnClickListener butMainHandler=new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+        //    Intent intent=new inten(getApplicationContext())
+            Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+            startActivity(intent);
+
+        }
+    };
 }
