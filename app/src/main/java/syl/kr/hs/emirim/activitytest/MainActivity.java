d51501e9c734fe5cf02c274010e537939e4bb6cf
soyunlee882 <soyunlee882@gmail.com>
2017-04-13 20:50:06 +0900

    #5

--- app/src/main/java/syl/kr/hs/emirim/activitytest/MainActivity.java
+++ app/src/main/java/syl/kr/hs/emirim/activitytest/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editName,editTel;
+
 
 
     @Override
@@ -16,12 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button butMain=(Button)findViewById(R.id.but_main);
         butMain.setOnClickListener(butMainHandler);
+        editName=(EditText)findViewById(R.id.edit_name);
+        editTel=(EditText)findViewById(R.id.edit_tel);
+
     }
     View.OnClickListener butMainHandler=new View.OnClickListener(){
         @Override
         public void onClick(View view){
         //    Intent intent=new inten(getApplicationContext())
             Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+            String name=editName.getText().toString();
+            String tel=editTel.getText().toString();
+            intent.putExtra("name",name);
+            intent.putExtra("tel",tel);
+
             startActivity(intent);
 
         }
